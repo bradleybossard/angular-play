97a16e464cad48aa0be3d81bd5826f18274a9c16
Bradley Bossard <bradleybossard@gmail.com>
2014-04-22 21:48:02 -0700

    Initial framework as provided by the angular generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularPlayApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
